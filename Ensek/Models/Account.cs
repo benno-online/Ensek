77f1a15beabaa0075339a632346d0adadac45b19
Leah Bentley <leah@benno-online.com>
2019-09-13 10:43:21 +0800

    Initial .net core api project creation with initial models

---
+++ Ensek/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ensek.Models
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Ensek/Models/MeterReading.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ensek.Models
+{
+    public class MeterReading
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public DateTime MeterReadingDateTime { get; set; }
+        public string MeterReadValue { get; set; }
+
+    }
+}
