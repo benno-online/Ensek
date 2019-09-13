40d110c3f9302d9e77b3557319ed4368cb21c881
Leah Bentley <leah@benno-online.com>
2019-09-13 14:02:14 +0800

    Added a front end html file

--- Ensek/Startup.cs
+++ Ensek/Startup.cs
@@ -43,6 +43,8 @@ namespace Ensek
                 app.UseHsts();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
