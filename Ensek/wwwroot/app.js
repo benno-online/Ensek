692a8d384b6e0ca48ab661cf43bf6f292f65a9e7
Leah Bentley <leah@benno-online.com>
2019-09-14 09:51:28 +0800

    Upload csv kind of works now :)

--- Ensek/wwwroot/app.js
+++ Ensek/wwwroot/app.js
@@ -1,31 +1,48 @@
 var app = angular.module('Ensek', ['ngFileUpload'])
-app.controller('MainCtrl', function ($scope, $window) {
+app.controller('MainCtrl', function ($scope, $window, $http) {
     $scope.SelectFile = function (file) {
         $scope.SelectedFile = file;
     };
+
+    $scope.SuccessCount = 0;
+    $scope.FailCount = 0;
+
+    function AddAccount() {
+        var data = {
+            AccountId: $scope.AccountId,
+            FirstName: $scope.FirstName,
+            LastName: $scope.LastName
+        };
+        $http.post('/api/account', JSON.stringify(data), ).then(function (response) {
+
+        if (response.data)
+
+            $scope.msg = "Post Data Submitted Successfully!";
+            $scope.SuccessCount++;
+
+        }, function (response) {
+
+            $scope.msg = response.data;
+            $scope.FailCount++;
+        });
+    };
+
     $scope.Upload = function () {
         var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
         if (regex.test($scope.SelectedFile.name.toLowerCase())) {
             if (typeof (FileReader) != "undefined") {
                 var reader = new FileReader();
                 reader.onload = function (e) {
-                    var accounts = new Array();
-                    var rows = e.target.result.split("\r\n");
-                    for (var i = 0; i < rows.length; i++) {
-                        var cells = rows[i].split(",");
-                        if (cells.length > 1) {
-                            var account = {};
-                            account.AccountId = cells[0];
-                            account.FirstName = cells[1];
-                            account.LastName = cells[2];
-                            accounts.push(account);
-                            $scope.$apply(function () {
-                                $scope.Accounts = accounts;
-                                $scope.IsVisible = true;
-                            });
-                        }
+                    var lines = e.target.result.split("\r\n");
+                    var result = [];
+                    var headers = lines[0].split(",");
+                    for (var i = 1; i < lines.length; i++) {
+                        var currentline = lines[i].split(",");
+                        $scope.AccountId = currentline[0];
+                        $scope.FirstName = currentline[1];
+                        $scope.LastName = currentline[2];
+                        AddAccount();
                     }
-
                 }
                 reader.readAsText($scope.SelectedFile);
             } else {
@@ -37,35 +54,9 @@ app.controller('MainCtrl', function ($scope, $window) {
     }
 });
 
-    //function postAccount () {
-    //    $http({
-    //        method: 'POST',
-    //        url: 'api/account',
-    //        data: $scope.Accounts,
-    //        headers: { 'Content-Type': 'application/json' }
-    //    }).then(function successCallback(response) {
-    //        $scope.output_msg = response.data;
-    //        alert("User has created Successfully")
-    //    }, function errorCallback(response) {
-    //        alert("Error while creating account. Try again!");
 
-    //    });
 
-    //};
 
-    //var lines = e.target.result.split("\r\n");
-    //var result = [];
-    //var headers = lines[0].split(",");
-    //for (var i = 1; i < lines.length; i++) {
-    //    var obj = {};
-    //    var currentline = lines[i].split(",");
-    //    for (var j = 0; j < headers.length; j++) {
-    //        obj[headers[j]] = currentline[j];
-    //    }
-    //    result.push(obj);
-    //}
-    //// do i need this as a json object??
-    //$scope.Accounts = JSON.stringify(result); //JSON
 
 
 
